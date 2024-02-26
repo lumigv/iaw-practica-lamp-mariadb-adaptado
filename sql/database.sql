e4ab35b6a71a53a1458b0ffd55c2b0d21c669abc
Luis Miguel  García Velázquez <luis@iesmhp.local>
2024-02-26 14:33:32 +0100

    Eliminamos actualizamos

--- sql/database.sql
+++ sql/database.sql
@@ -1,5 +1,4 @@
-USE lamp_db;
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users2 (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   surname1 VARCHAR(100) NOT NULL,
   surname2 VARCHAR(100),
@@ -8,5 +7,5 @@ CREATE TABLE IF NOT EXISTS users (
   email VARCHAR(100) UNIQUE NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO users (surname1, surname2, name, age, email) VALUES('Colmenar', 'Viejo', 'Bonifacia', '25', 'bonifacia@gmail.com');
+INSERT INTO users (surname1, surname2, name, age, email) VALUES('Colmenar', 'Viejo', 'Bonifacia', 25, 'bonifacia@gmail.com');
 
