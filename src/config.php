53708f629f5a7ad4a524a546398704697d16bc2b
lumigv <lumigv@gmail.com>
2024-02-28 00:05:37 +0100

    Update config.php

--- src/config.php
+++ src/config.php
@@ -5,6 +5,14 @@ define('DB_NAME', getenv('DB_NAME'));
 define('DB_USER', getenv('DB_USER'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
+/*
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'database_name_here');
+define('DB_USER', 'username_here');
+define('DB_PASSWORD', 'password_here');
+*/
+
+
 $mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 if (mysqli_connect_errno()) {
     printf('Falló la conexión: %s\n', mysqli_connect_error());
